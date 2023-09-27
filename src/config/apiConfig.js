f534fd53b60aaf572a3496e622eef618aad5847b
Animesh kumbhakar <animeshkumbhakarhit2@gmail.com>
2023-09-28 01:56:53 +0530

    added vercel

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,2 +1,3 @@
 const PORT = "5040";
-export const BaseUrl = `http://localhost:${PORT}/api/v1`;
+// export const BaseUrl = `http://localhost:${PORT}/api/v1`;
+export const BaseUrl = "https://berozzgar-backend.vercel.app/api/v1";
