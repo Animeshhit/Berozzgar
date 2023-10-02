d08801820e89afe71e60d6c359bbfbb6f8391444
Animesh kumbhakar <animeshkumbhakarhit2@gmail.com>
2023-10-03 01:46:29 +0530

    added port

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,4 +2,5 @@ const PORT = "3030";
 // export const BaseUrl = `http://localhost:${PORT}/api/v1`;
 
 // export const BaseUrl = "https://berozzgar-backend.vercel.app/api/v1";
-export const BaseUrl = "https://curious-sock-fish.cyclic.cloud/api/v1";
+// export const BaseUrl = "https://curious-sock-fish.cyclic.cloud/api/v1";
+export const BaseUrl = "https://wild-tan-alligator-kilt.cyclic.cloud/api/v1";
