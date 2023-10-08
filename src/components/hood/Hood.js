952271900e94d08cb8ecca5a1848607b801d5cf9
Animesh kumbhakar <animeshkumbhakarhit2@gmail.com>
2023-10-08 22:24:00 +0530

    added hod section

--- src/components/hood/Hood.js
+++ src/components/hood/Hood.js
@@ -1,24 +1,74 @@
+import { useState, useRef, useEffect } from "react";
+import { BaseUrl } from "../../config/apiConfig";
+import { toast } from "react-toastify";
+
+//swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+
+import "swiper/css";
+
 const Hood = () => {
+  const [messages, setMessages] = useState(null);
+  const HoodMessages = async () => {
+    try {
+      let APIREQ = await fetch(`${BaseUrl}/hodmessages`);
+      let APIRES = await APIREQ.json();
+      console.log(APIRES);
+      setMessages(APIRES);
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
+  useEffect(() => {
+    HoodMessages();
+  }, []);
   return (
     <>
       <div className="w-full bg-zinc-800 text-white">
         <div className="text-center px-4">
-          <p className="py-3 text-xs capitalize leading-relaxed">
-            Created With Love ❤️ by{" "}
-            <a
-              className="text-blue-300"
-              href="https://www.linkedin.com/in/sourav-majee/"
-            >
-              Sourav Majee(EE-2)
-            </a>{" "}
-            &{" "}
-            <a
-              className="text-red-300"
-              href="https://www.linkedin.com/in/saikat-malik-56b1a0254/"
-            >
-              Saikat Malik(EE-2)
-            </a>
-          </p>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            autoplay={{
+              delay: 3500,
+              disableOnInteraction: false,
+            }}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            className="mySwiper"
+            modules={[Autoplay]}
+          >
+            <SwiperSlide className="py-3 text-xs capitalize leading-relaxed">
+              Created With Love ❤️ by{" "}
+              <a
+                className="text-blue-300"
+                href="https://www.linkedin.com/in/sourav-majee/"
+              >
+                Sourav Majee(EE-2)
+              </a>{" "}
+              &{" "}
+              <a
+                className="text-red-300"
+                href="https://www.linkedin.com/in/saikat-malik-56b1a0254/"
+              >
+                Saikat Malik(EE-2)
+              </a>
+            </SwiperSlide>
+            {messages &&
+              messages.map((msg, index) => {
+                return (
+                  <SwiperSlide
+                    key={msg._id}
+                    className="py-3 msg text-xs capitalize leading-relaxed capitalize text-white-800"
+                  >
+                    ℹ️ℹ️{msg.message}
+                  </SwiperSlide>
+                );
+              })}
+          </Swiper>
         </div>
       </div>
     </>
