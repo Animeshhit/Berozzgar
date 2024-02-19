da7af686d1cbcf94b11a24acbede743ded9ccdb6
Animesh kumbhakar <animeshkumbhakarhit2@gmail.com>
2024-02-19 17:11:21 +0530

    sf

---
+++ src/app/auth/login/page.js
@@ -0,0 +1,11 @@
+const Page = () => {
+  return (
+    <>
+      <p className="text-center text-white capitalize font-semibold text-3xl mt-8">
+        Kaam chalu hai meri jaan 🔥🔥
+      </p>
+    </>
+  );
+};
+
+export default Page;
