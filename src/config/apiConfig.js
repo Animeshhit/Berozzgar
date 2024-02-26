5dfc6ed33869713d3408d49ff8b1258d05e8c222
Animesh kumbhakar <animeshkumbhakarhit2@gmail.com>
2024-02-26 10:39:40 +0530

    added server url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,3 +1,4 @@
-const BaseUrl = "http://localhost:3040/api/v2";
+// const BaseUrl = "http://localhost:3040/api/v2";
+const BaseUrl = "https://be-backend-two.vercel.app/api/v2";
 
 export default BaseUrl;
