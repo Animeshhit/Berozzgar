0a45d7e636c7e3c6d73e4bb1aa2400abd2aacdeb
Animesh kumbhakar <animeshkumbhakarhit2@gmail.com>
2024-03-10 13:17:00 +0530

    year page bug fixed and mobile version created

--- src/app/get-started/page.js
+++ src/app/get-started/page.js
@@ -5,7 +5,6 @@ import { useState } from "react";
 
 const Page = () => {
   const [mainAnimatedButton, setMainAnimatedButton] = useState(false);
-  const [isClicked, setIsClicked] = useState(false);
   const [animation, setAnimation] = useState({
     first: false,
     second: false,
@@ -31,42 +30,17 @@ const Page = () => {
     }, 2000);
   };
 
-  const handleMouseOut = () => {
-    if (isClicked) return;
-    setTimeout(() => {
-      setAnimation({ ...animation, last: false });
-    }, 300);
-
-    setTimeout(() => {
-      setAnimation({ ...animation, third: false, last: false });
-    }, 900);
-    setTimeout(() => {
-      setAnimation({ ...animation, last: false, second: false, third: false });
-    }, 1300);
-
-    setTimeout(() => {
-      setAnimation({ last: false, first: false, second: false, third: false });
-    }, 1900);
-
-    setTimeout(() => {
-      setMainAnimatedButton(false);
-    }, 2400);
-  };
-
   return (
     <>
-      <div className="container mx-auto px-5">
+      {/* for Desktop user  */}
+      <div className="container mx-auto px-5 hidden md:block">
         <div className="flex items-start justify-center gap-200 mt-12">
           <button
             type="button"
-            onClick={() => {
-              setIsClicked(true);
-            }}
-            onMouseEnter={handleMouseIn}
-            onMouseLeave={handleMouseOut}
-            className={`${isClicked ? "bg-zinc-800" : "bg-accent"} ${
-              mainAnimatedButton ? "afterDotactive" : ""
-            } hover:bg-zinc-800 text-2xl transition py-3 px-6 rounded-full flex items-center gap-3 afterDot`}
+            onClick={handleMouseIn}
+            className={`bg-accent ${
+              mainAnimatedButton ? "afterDotactive gap-6" : ""
+            } text-xl font-semibold mt-0.5 py-3 px-6 rounded-full flex items-center gap-3 transition-all afterDot`}
           >
             <span>Choose Your Year</span>
             <ion-icon name="arrow-forward-outline"></ion-icon>
@@ -76,74 +50,112 @@ const Page = () => {
               <div
                 className={`dot dot-one ${
                   animation.first ? "active" : ""
-                } w-5 h-5 bg-accent rounded-full`}
+                } w-5 h-5  rounded-full`}
               ></div>
               <Link
                 href="/year/first"
-                className={`bg-zinc-800 py-3 rounded-full px-24 transition-opacity duration-150 block ${
+                className={`bg-zinc-800 py-3 rounded-full px-4 transition-all duration-150 block ${
                   animation.first ? "opacity-1" : "opacity-0"
-                } text-2xl ${
-                  animation.first ? "bg-accent transition-colors delay-500" : ""
+                } text-xl font-bold ${
+                  animation.first
+                    ? "bg-accent transition-colors delay-500 padding-x"
+                    : ""
                 }`}
               >
-                First Year
+                1st Year
               </Link>
             </div>
             <div className="flex items-center mb-12 gap-[100px]">
               <div
                 className={`dot dot-two ${
                   animation.second ? "active" : ""
-                } w-5 h-5 bg-accent rounded-full`}
+                } w-5 h-5 rounded-full`}
               ></div>
               <Link
                 href="/year/first"
-                className={`bg-zinc-800 py-3 rounded-full px-24 transition-opacity duration-150 block ${
+                className={`bg-zinc-800 py-3 rounded-full px-4 transition-all duration-150 block ${
                   animation.second ? "opacity-1" : "opacity-0"
-                } text-2xl ${
+                } text-xl font-bold ${
                   animation.second
-                    ? "bg-accent transition-colors delay-500"
+                    ? "bg-accent transition-colors delay-500 padding-x"
                     : ""
                 }`}
               >
-                Second Year
+                2nd Year
               </Link>
             </div>
             <div className="flex items-center mb-12 gap-[100px]">
               <div
                 className={`dot dot-three ${
                   animation.third ? "active" : ""
-                } w-5 h-5 bg-accent rounded-full`}
+                } w-5 h-5 rounded-full`}
               ></div>
               <Link
                 href="/year/first"
-                className={`bg-zinc-800 py-3 rounded-full px-24 transition-opacity duration-150 block ${
+                className={`bg-zinc-800 py-3 rounded-full px-4 transition-all duration-150 block ${
                   animation.third ? "opacity-1" : "opacity-0"
-                } text-2xl ${
-                  animation.third ? "bg-accent transition-colors delay-500" : ""
+                } text-xl font-bold ${
+                  animation.third
+                    ? "bg-accent transition-colors delay-500 padding-x"
+                    : ""
                 }`}
               >
-                Third Year
+                3rd Year
               </Link>
             </div>
             <div className="flex items-center mb-12 gap-[100px]">
               <div
                 className={`dot dot-four ${
                   animation.last ? "active" : ""
-                } w-5 h-5 bg-accent rounded-full`}
+                } w-5 h-5 rounded-full`}
               ></div>
               <Link
                 href="/year/first"
-                className={`bg-zinc-800 py-3 rounded-full px-24 transition-opacity duration-150 block ${
+                className={`bg-zinc-800 py-3 rounded-full px-4 transition-all duration-150 block ${
                   animation.last ? "opacity-1" : "opacity-0"
-                } text-2xl ${
-                  animation.last ? "bg-accent transition-colors delay-500" : ""
+                } text-xl font-bold ${
+                  animation.last
+                    ? "bg-accent transition-colors delay-500 padding-x"
+                    : ""
                 }`}
               >
-                Fourth Year
+                4th Year
               </Link>
             </div>
           </div>
         </div>
+      </div>
+      {/* for mobile or tablet user  */}
+
+      <div className="container px-4 mx-auto mt-14">
+        <Link
+          href="/year/first"
+          data-year="1"
+          className="py-3 px-4 bg-accent max-w-sm block mx-auto  w-full rounded-full text-center font-semibold  icon relative"
+        >
+          First Year
+        </Link>
+        <Link
+          href="/year/first"
+          data-year="2"
+          className="py-3 px-4 bg-accent max-w-sm block mx-auto  w-full rounded-full text-center font-semibold mt-5  icon relative"
+        >
+          Second Year
+        </Link>
+        <Link
+          href="/year/first"
+          data-year="3"
+          className="py-3 px-4 bg-accent max-w-sm block mx-auto  w-full rounded-full text-center font-semibold mt-5  icon relative"
+        >
+          Third Year
+        </Link>
+        <Link
+          href="/year/first"
+          data-year="4"
+          className="py-3 px-4 bg-accent max-w-sm block mx-auto  w-full rounded-full text-center font-semibold mt-5  icon relative"
+        >
+          Fourth Year
+        </Link>
       </div>
     </>
   );
